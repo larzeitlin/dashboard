48aa6fc0616fb4361592758aaad637d5195311a6
Luke <larzeitlin@gmail.com>
2023-05-29 15:06:21 +0100

    first commit

---
+++ cljs/script.cljs
@@ -0,0 +1,13 @@
+(require
+ '[reagent.core :as r]
+ '[reagent.dom :as rdom])
+
+(def state (r/atom {:clicks 0}))
+
+(defn my-component []
+ [:div
+  [:p "Clicks: " (:clicks @state)]
+  [:p [:button {:on-click #(swap! state update :clicks inc)}
+       "Click me!"]]])
+
+(rdom/render [my-component] (.getElementById js/document "app"))
